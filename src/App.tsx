a085004c336b39931b92deb6a306decc1389d1bd
dylanwoo <13128589345@163.com>
2024-07-03 20:57:01 +0800

    init demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-import WebApp from '@twa-dev/sdk'
+import "./App.css";
+
+import { MainButton, useShowPopup } from "@vkruglikov/react-telegram-web-app";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const showPopup = useShowPopup();
 
+  const handleClick = () =>
+    showPopup({
+      message: "Hello, I am popup",
+    });
   return (
     <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TWA + Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-      {/*  */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
+      <button onClick={handleClick}>Show Popup</button>
+      <MainButton text="SHOW POPUP" onClick={handleClick} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
